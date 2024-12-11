f0fa5489c903ba2c73aa298d7c5f1336fe09fbc3
gsizer <gsizer@gmail.com>
2024-12-10 21:16:56 -0700

    activetask updates, add screenshot
    add newline between records in file,
    update bindings and dictionary access,
    include screenshot

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Quest Log
 
 A Quest-Log Composer using [Object-Generator](https://github.com/gsizer/Object-Generator) and [DiceRoller](https://github.com/gsizer/DiceRoller).
+
+![Screenshot of the Questlog module](./img/screenshot.png)
--- quest_log.gd
+++ quest_log.gd
@@ -48,20 +48,26 @@ func _on_btn_load_logbook_pressed() -> void:
 	load_questbook()
 
 func _on_btn_add_task_pressed() -> void:
+	# copy template dictionary
 	ActiveTask = NewTask.duplicate()
-	Logbook.get_or_add(Logbook.size(), ActiveTask)
-	listTasklist.add_item( ActiveTask.get("Name") )
+	# use the logbook size() as the index for the new task
+	Logbook[ Logbook.size() ] = ActiveTask
+	listTasklist.add_item( ActiveTask["Name"] )
+	# store the active index
 	_activeIdx = listTasklist.get_child_count()
+	# select the new item
 	listTasklist.select(_activeIdx)
 
 func _on_btn_del_task_pressed() -> void:
+	# clear the input fields
 	leTitle.text = ""
 	leBrief.text = ""
 	optTasktype.select( 0 )
 	teDescription.text = ""
+	# drop the data from memory
 	listTasklist.remove_item(_activeIdx)
-	_activeIdx = -1
 	ActiveTask.clear()
+	_activeIdx = -1
 
 func _on_itemlist_tasklist_item_selected(index: int) -> void:
 	ActiveTask = Logbook.get(index)
@@ -78,12 +84,14 @@ func _on_le_title_text_changed(new_text):
 	taskName = new_text
 
 func _on_le_title_text_submitted(new_text):
+	taskName = new_text
 	ActiveTask["Name"] = taskName
 
 func _on_le_brief_text_changed(new_text):
 	taskSummary = new_text
 
 func _on_le_brief_text_submitted(new_text):
+	taskSummary = new_text
 	ActiveTask["Summary"] = taskSummary
 
 func _on_opt_tasktype_item_selected(index):
@@ -92,20 +100,29 @@ func _on_opt_tasktype_item_selected(index):
 func _on_te_description_text_changed() -> void:
 	taskDescription = teDescription.text
 
+func _on_te_description_caret_changed():
+	taskDescription = teDescription.text
+
 func _on_btn_task_update_pressed():
-# update UI
+	# update UI
 	listTasklist.set_item_text(_activeIdx, taskName)
 	mdDescription.markdown_text = taskDescription
-# update data model
+	# update data model
 	ActiveTask["Name"] = taskName
 	ActiveTask["Summary"] = taskSummary
 	ActiveTask["Tasktype"] = taskType
 	ActiveTask["Description"] = taskDescription
-# update collection
+	# update collection
 	Logbook[_activeIdx] = ActiveTask
 
 func _on_btn_task_reset_pressed():
-	pass # Replace with function body.
+	# get the stored values
+	ActiveTask = Logbook[_activeIdx]
+	# copy data back to input fields
+	leTitle.text = ActiveTask["Name"]
+	leBrief.text = ActiveTask["Summary"]
+	optTasktype.select( ActiveTask["Tasktype"] )
+	teDescription.text = ActiveTask["Description"]
 
 func exit_app()->void:
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
@@ -123,6 +140,8 @@ func load_questbook()->void:
 func save_questbook()->void:
 	_file_mode = FileAccess.WRITE
 	var fout := FileAccess.open(_book_file, _file_mode)
-	var _logbook = JSON.stringify(Logbook)
-	fout.store_string(_logbook)
+	#var _logbook = JSON.stringify(Logbook)
+	for _quest in Logbook as Dictionary:
+		var _out = JSON.stringify(_quest)
+		fout.store_string(_out)
 	fout.close()
